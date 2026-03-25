cb4aff740a6c286982407d9d696ef8692202505f
sachitrrazdan1710 <109738501+sachitrrazdan1710@users.noreply.github.com>
2026-03-25 23:23:09 +0530

    Enhance trivy_scan function with additional options

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,9 @@
-def call(){
-  sh "trivy fs ."
+def call() {
+    sh """
+        trivy fs . \
+        --skip-db-update \
+        --severity HIGH,CRITICAL \
+        --exit-code 1 \
+        --timeout 5m
+    """
 }
