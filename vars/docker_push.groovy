2ab35536fc6a4ec2a1700bbb9d5329c32e140561
sachitrrazdan1710 <109738501+sachitrrazdan1710@users.noreply.github.com>
2026-04-08 00:36:36 +0530

    Fix credentials ID in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-   withCredentials([usernamePassword(credentialsId: 'dockerHubCred',
+   withCredentials([usernamePassword(credentialsId: 'docker',
                                      passwordVariable: 'dockerHubPass', 
                                      usernameVariable: 'dockerHubUser'
         )]){
