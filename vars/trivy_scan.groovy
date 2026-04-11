9c7b569098f2447bd44adc05f49e208c133a6941
sachitrrazdan1710 <109738501+sachitrrazdan1710@users.noreply.github.com>
2026-04-11 10:58:45 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -2,7 +2,11 @@ def call() {
     sh """
         trivy fs . \
         --severity HIGH,CRITICAL \
+        --scanners vuln \
         --cache-dir /tmp/trivy-cache \
-         --skip-db-update
+        --skip-db-update \
+        --skip-dirs node_modules \
+        --skip-dirs frontend/node_modules \
+        --skip-dirs backend/node_modules
     """
 }
