707bbef10d1a4ecfe623f4000367a3e06317af95
sachitrrazdan1710 <109738501+sachitrrazdan1710@users.noreply.github.com>
2026-04-12 19:15:27 +0530

    Modify docker build command to exclude --pull option
    Remove the --pull option from the docker build command.

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -3,8 +3,11 @@ def call(String ProjectName, String ImageTag, String DockerHubUser) {
 
     retry(3) {
         sh """
+            set -e
+            echo "Pulling base image first..."
+            docker pull node:18-alpine
             echo "Building Docker image: ${image}"
-            docker build --pull -t ${image} .
+            docker build -t ${image} .
         """
     }
 }
