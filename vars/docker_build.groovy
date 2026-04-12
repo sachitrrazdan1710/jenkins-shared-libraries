0e45d24bbdf83943d9269aee82553dfa45341936
sachitrrazdan1710 <109738501+sachitrrazdan1710@users.noreply.github.com>
2026-04-12 19:14:59 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,10 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String DockerHubUser) {
+    def image = "${DockerHubUser}/${ProjectName}:${ImageTag}"
+
+    retry(3) {
+        sh """
+            echo "Building Docker image: ${image}"
+            docker build --pull -t ${image} .
+        """
+    }
 }
